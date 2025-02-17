ccf919dc355e9b3636387f7bdb76b81caa7d3e9a
aliii390 <alli.said.errahmani@gmail.com>
2025-02-17 11:10:31 +0100

    ./

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react";
 
 
 
 function App() {
   return (
     <div className="min-h-screen ">
-    
+    <Analytics/>
       <Header />
       <main >
         <Hero />
